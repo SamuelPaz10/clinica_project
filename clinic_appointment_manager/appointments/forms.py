8c995d043abd8e493fa278b475d85a0c71c283a7
JAAO97 <jeseramaya@gmail.com>
2024-05-04 22:37:22 -0600

    Prueba

--- clinic_appointment_manager/appointments/forms.py
+++ clinic_appointment_manager/appointments/forms.py
@@ -4,4 +4,4 @@ from .models import Appointment
 class AppointmentForm(ModelForm):
     class Meta:
         model = Appointment
-        fields = ['patient', 'reason', 'available_datetime', 'status']
+        fields = ['patient', 'doctor', 'reason', 'available_datetime', 'status']
